bdc6336e7e7086ae6a73fd27a22aefdc20478ee8
Darshan J <itzdarsh@users.noreply.github.com>
2022-09-07 16:39:53 +0530

    Update prepl_config.py

--- prepl_config.py
+++ prepl_config.py
@@ -1,7 +1,6 @@
-ignoreDB = ["test"]
-doDB = ["mydb","newDB"]
-
-
 source = "mongodb+srv://root:****@mflix.*****.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true"
 
 target = "mongodb://user:****@127.0.0.1:27017/"
+
+ignoreDB = ["test"]
+doDB = ["mydb","newDB"]
